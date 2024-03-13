a78be8e6fb70395bb483471af9d026e6df3e489a
DigitalCoin593 <114309352+DigitalCoin593@users.noreply.github.com>
2024-03-13 07:37:42 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    radio.setTransmitPower(4)
+    music.play(music.stringPlayable("- D F D G B B E ", input.magneticForce(Dimension.Strength)), music.PlaybackMode.LoopingInBackground)
+    radio.sendString("" + ((0 as any) == (1.0154932 as any)))
+    music.ringTone(330)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
